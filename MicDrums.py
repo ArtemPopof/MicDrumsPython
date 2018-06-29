fbfad5f164fe197ceb8cce90a98fccdd6ec057fc
ArtemPopof <Deviling42@gmail.com>
2018-06-29 18:38:04 +0300

    algorithm completed

--- MicDrums.py
+++ MicDrums.py
@@ -36,12 +36,10 @@ def mapDrums(clip):
     return separateDrums(positionsWithoutDublicates, typesWithoutDublicates)
 
 def processBlock(block, offset):
-    maxAmplitude = max(block)
-    minAmplitude = min(block)
+    maxAmplitude = block.max()
+    minAmplitude = block.min()
 
-    loudestSample1 = max(abs(maxAmplitude[0]), abs(minAmplitude[0]))
-    loudestSample2 = max(abs(maxAmplitude[1]), abs(minAmplitude[1]))
-    loudestSample = max(loudestSample1, loudestSample2)
+    loudestSample = max(abs(maxAmplitude.max()), abs(minAmplitude.min()))
 
     if loudestSample >= snareGateValue:
         print("Snare drum on ", offset)
@@ -50,14 +48,16 @@ def processBlock(block, offset):
         print("Kick drum on ", offset)
         return offset, "kick"
 
-    return [], []
+    return None, None
 
 def removeDublicates(drums, types):
     needMoreIterations = True
     while needMoreIterations:
         needMoreIterations = False
         for i in range(0, len(drums) - 1):
-            if drums[i+1] - drums[i] <= maxDrumWidth:
+            nextSample = drums[i+1]
+            currentSample = drums[i]
+            if nextSample - currentSample <= maxDrumWidth:
                 if types[i] == "kick":
                     types.pop(i)
                     drums.pop(i)
@@ -82,16 +82,66 @@ def separateDrums(positions, types):
 
     return kicks, snares
 
+def convertSamplesToTime(kicksPosition, snaresPosition, rate):
+    for i in range(0, len(kicksPosition)):
+        kicksPosition[i] = kicksPosition[i] * 1.0 / rate
+    for i in range(0, len(snaresPosition)):
+        snaresPosition[i] = snaresPosition[i] * 1.0 / rate
+
+    return kicksPosition, snaresPosition
+
+def generate_drum_beat(drumSample, positions, rate):
+    generatedClip = np.array([[0.0,0.0]] * (int(positions[len(positions) - 1] * rate) + len(drumSample)))
+    for i in range(0, len(positions)):
+        generatedClip = add_drum_to_clip(generatedClip, drumSample, positions[i], rate)
+    return generatedClip
+
+def add_drum_to_clip(clip, drumSample, time, rate):
+    firstSampleIdx = int(time * rate)
+    for i in range(firstSampleIdx, min(len(clip), firstSampleIdx + len(drumSample))):
+        nextSample = drumSample[i - firstSampleIdx]
+        clip[i] = nextSample
+    return clip
+
+def mix(firstTrack, secondTrack):
+    if firstTrack.size > secondTrack.size:
+        mix = firstTrack
+        minSize = int(secondTrack.size / 2 - 1)
+    else:
+        mix = secondTrack
+        minSize = int(firstTrack.size / 2 - 1)
+
+    for i in range(0, minSize):
+        mix[i] = firstTrack[i] + secondTrack[i]
+    return mix
+
 
 rate, s = wavfile.read('/home/whale/Desktop/rawBeat.wav')
 
 s = normalize(s)
 
-kicks, snares = mapDrums(s.tolist())
+kicks, snares = mapDrums(s)
 print("kicks: ", kicks)
 print("snares: ", snares)
 
-plt.plot(s)
-plt.show()
+# we have sample numbers where all kicks and snares is
+# now we need to make clip with kicks and snares sounds
 
-wavfile.write('beatStage1.wav', rate, s)
+# convert sample numbers to time according to input clip SampleRate
+kicksTime, snaresTime = convertSamplesToTime(kicks, snares, rate)
+print("kicksT: ", kicksTime)
+print("snaresT: ", snaresTime)
+
+# get kick and snares audio clips (must be prepared before loading)
+kickRate, kickClip = wavfile.read('/home/whale/Desktop/kickClip.wav')
+snareRate, snareClip = wavfile.read('/home/whale/Desktop/snareClip.wav')
+
+generatedKicks = generate_drum_beat(kickClip, kicks, kickRate)
+generatedSnares = generate_drum_beat(snareClip, snares, snareRate)
+
+wavfile.write("generatedKickBeat.wav", kickRate, generatedKicks)
+wavfile.write("generatedSnareBeat.wav", snareRate, generatedSnares)
+wavfile.write("generatedMixtape.wav", kickRate, mix(generatedKicks, generatedSnares))
+
+plt.plot(generatedKicks)
+plt.show()
